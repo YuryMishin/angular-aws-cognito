9b5944997f8fe7026a8cf68ce0544abc9a744537
Yury Mishin <yury.mishin96@gmail.com>
2019-08-26 19:12:47 +0300

    Config core module

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MaterialModule } from './material.module';
+import { ApiService } from './services/api.service';
+import { CognitoService } from './services/cognito.service';
+import { AwsConfigurationService } from './services/aws-configuration.service';
+import { AwsService } from './services/aws.service';
+import { AuthGuardService } from './services/auth-guard.service';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    MaterialModule,
+  ],
+  providers: [ApiService, CognitoService, AwsConfigurationService, AwsService, AuthGuardService]
 })
-export class CoreModule { }
+export class CoreModule {
+}
